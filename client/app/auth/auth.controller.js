c668df86a04cda3255289fe108f73bb08446e048
remimi <remi.millet@brainify.it>
2016-06-01 09:41:04 +0200

    Buf fix on slack auth

--- client/app/auth/auth.controller.js
+++ client/app/auth/auth.controller.js
@@ -8,14 +8,15 @@ angular.module("bdc").controller("AuthController",
                 AuthService.login($stateParams.code).then(
                     function(success){
                         if(success.data.ok){
+                            AuthService.handleData(success.data);
                             if(success.data.team.id != CONFIG.teamID){
                                 console.log('BAD TEAM');
                                 AuthService.logout();
+                                //Go to another page
+                            } else {
+                                console.log($rootScope.context.user);
+                                $state.go('app.deals');
                             }
-
-                            AuthService.handleData(success.data);
-                            console.log($rootScope.context.user);
-                            $state.go('app.deals');
 
                         } else {
                             console.log('Error login')
