1646300250808f1f29f1b6b31c10fbc2cb28c0a8
remimillet@gmail.com <Lgiw29kf>
2016-11-24 23:51:13 +0100

    Prod config for constants

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -3,7 +3,7 @@
 angular.module("bdc")
     .constant("CONFIG",{
         teamID : "T0PSA99BN",
-        slackOAuth : ""
+        slackOAuth : "authorize?scope=identity.basic,identity.email&client_id=23894315396.55118495363"
     })
     .constant("SKILLS" , {
         availableSkills : ['UI','UX',"MOTION-DESIGN", "TYPOGRAPHIE","ILLUSTRATION","PHOTOGRAPHIE","FRONT-END"]
